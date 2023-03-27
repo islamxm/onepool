67cd346b59ae00d7c4cc3d55c3d7dc30b2a86418
Islam <isxmad@gmail.com>
2023-03-27 21:33:07 +0300

    conc pools

---
+++ components/StepsMob/StepsMob.tsx
@@ -0,0 +1,37 @@
+import styles from './StepsMob.module.scss';
+import {FC} from 'react';
+import { stepPropsTypes } from '@/pageModules/home/Steps/types';
+import Container from '../Container/Container';
+import BlockHead from '../BlockHead/BlockHead';
+
+const StepsMob:FC<stepPropsTypes> = ({
+    list,
+    title
+}) => {
+
+    return (
+        <div className={styles.wrapper}>
+            <Container>
+                <div className={styles.in}>
+                    <div className={styles.head}>
+                        <BlockHead
+                            isDark
+                            title={title}
+                            />
+                    </div>
+                    <div className={styles.body}>
+                        {
+                            list?.map((item,index) => (
+                                <div className={styles.item} key={index}>
+                                    <div className={styles.in}>
+                                        
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
