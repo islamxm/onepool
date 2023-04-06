9d38f2b7de1c2b0c073b700e90435a26b767f7b0
Islam <isxmad@gmail.com>
2023-04-06 19:21:03 +0300

    fix 06.04

--- pageModules/home/Prods/Prods.tsx
+++ pageModules/home/Prods/Prods.tsx
@@ -2,7 +2,7 @@ import styles from './Prods.module.scss';
 import BlockHead from '@/components/BlockHead/BlockHead';
 import Container from '@/components/Container/Container';
 import Tabs from '@/components/Tabs/Tabs';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Image from 'next/image';
 import img from '@/public/assets/prods-1.png';
 import MoreLink from '@/components/MoreLink/MoreLink';
@@ -11,26 +11,100 @@ import { childAnim } from '@/helpers/animObjects';
 import {motion} from 'framer-motion';
 import { parentAnim } from '@/helpers/animObjects';
 import { animWhileInView } from '@/helpers/animObjects';
+import img2_1 from '@/public/assets/prods-2-1.png';
+import img2_2 from '@/public/assets/prods-2-2.png';
+import img2_3 from '@/public/assets/prods-2-3.png';
+import img2_4 from '@/public/assets/prods-2-4.png';
+import img2_5 from '@/public/assets/prods-2-5.png';
+import img2_6 from '@/public/assets/prods-2-6.png';
+import img2_7 from '@/public/assets/prods-2-7.png';
 
-import img1 from '@/public/assets/prods-2-1.png';
-import img2 from '@/public/assets/prods-2-2.png';
-import img3 from '@/public/assets/prods-2-3.png';
-import img4 from '@/public/assets/prods-2-4.png';
-import img5 from '@/public/assets/prods-2-5.png';
-import img6 from '@/public/assets/prods-2-6.png';
-import img7 from '@/public/assets/prods-2-7.png';
+import img1_1 from '@/public/assets/prods-1-1.png';
+import img1_2 from '@/public/assets/prods-1-2.png';
+import img1_3 from '@/public/assets/prods-1-3.png';
+import img1_4 from '@/public/assets/prods-1-4.png';
+import img1_5 from '@/public/assets/prods-1-5.png';
+import img1_6 from '@/public/assets/prods-1-6.png';
+import img1_7 from '@/public/assets/prods-1-7.png';
+
+import img3_1 from '@/public/assets/prods-3-1.png';
+import img3_2 from '@/public/assets/prods-3-2.png';
+import img3_3 from '@/public/assets/prods-3-3.png';
+import img3_4 from '@/public/assets/prods-3-4.png';
+import img3_5 from '@/public/assets/prods-3-5.png';
+import img3_6 from '@/public/assets/prods-3-6.png';
+import img3_7 from '@/public/assets/prods-3-7.png';
+
+
 
 const tabsList = [
     {id: '1', label: 'бассейны'},
     {id: '2', label: 'спа'},
-    {id: '3', label: 'бани и сауны'},
+    {id: '3', label: 'бани и сауны'}
 ]
 
-
+{/* <AnimWrap>
+<motion.div variants={childAnim('bottom')} className={styles.body}>
+    <div className={styles.item}><Image src={img1} alt=""/></div>
+    <div className={styles.item}><Image src={img2} alt=""/></div>
+    <div className={styles.item}><Image src={img3} alt=""/></div>
+    <div className={styles.item}><Image src={img4} alt=""/></div>
+    <div className={styles.item}><Image src={img5} alt=""/></div>
+    <div className={styles.item}><Image src={img6} alt=""/></div>
+    <div className={styles.item}><Image src={img7} alt=""/></div>
+</motion.div>
+</AnimWrap> */}
 
 
 const Prods = () => {
     const [activeTab, setActiveTab] = useState('1')
+
+
+    const switchGallery = useCallback((activeTab: string) => {
+        switch(activeTab) {
+            case '2':
+                return (
+                    <div className={styles.body}>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_1} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_2} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_3} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_4} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_5} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_6} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img2_7} alt=""/></div>
+                    </div>
+                )
+            case '1':
+                return (
+                    <div className={styles.body}>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_1} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_2} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_3} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_4} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_5} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_6} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img1_7} alt=""/></div>
+                    </div>
+                )
+            case '3':
+                return (
+                    <div className={styles.body}>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_1} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_2} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_3} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_4} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_5} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_6} alt=""/></div>
+                        <div className={styles.item}><Image placeholder='blur' src={img3_7} alt=""/></div>
+                    </div>
+                )
+        }
+    }, [activeTab])
+
+
+
+
+
 
     return (
         <motion.div variants={parentAnim} {...animWhileInView} className={styles.wrapper}>
@@ -62,17 +136,10 @@ const Prods = () => {
                             </p>
                         </motion.div>
                     </AnimWrap>
-                    <AnimWrap>
-                        <motion.div variants={childAnim('bottom')} className={styles.body}>
-                            <div className={styles.item}><Image src={img1} alt=""/></div>
-                            <div className={styles.item}><Image src={img2} alt=""/></div>
-                            <div className={styles.item}><Image src={img3} alt=""/></div>
-                            <div className={styles.item}><Image src={img4} alt=""/></div>
-                            <div className={styles.item}><Image src={img5} alt=""/></div>
-                            <div className={styles.item}><Image src={img6} alt=""/></div>
-                            <div className={styles.item}><Image src={img7} alt=""/></div>
-                        </motion.div>
-                    </AnimWrap>
+                    {
+                        switchGallery(activeTab)
+                    }
+                    
                     <AnimWrap className={styles.action}>
                         <motion.div variants={childAnim('bottom')} className={styles.action}>
                             <MoreLink
