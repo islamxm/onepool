111087909a0a711c6f2ff58285e5692c32b8bd4b
Islam <isxmad@gmail.com>
2023-04-21 21:01:29 +0300

    f

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,16 @@ export default function Document() {
             `
           }}
           />
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,u){
+              var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+              var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+            })(window,document,'https://bitrix24.key-corp.ru/upload/crm/site_button/loader_2_arq710.js');
+            `
+          }}
+          />
       </body>
     </Html>
   )
