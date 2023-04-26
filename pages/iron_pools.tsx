ba9718e7e7a4fb3e0a9b2e4ac91d78c12a6bf775
Islam Madaminov <isxmad@gmail.com>
2023-04-26 16:26:42 +0300

    metric

--- pages/iron_pools.tsx
+++ pages/iron_pools.tsx
@@ -399,6 +399,27 @@ const IronPage = () => {
                 <Faq list={faqList}/>
                 <Footer/>
             </PageLayout>
+
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                 
+                    ym(93342060, "init", {
+                         clickmap:true,
+                         trackLinks:true,
+                         accurateTrackBounce:true,
+                         webvisor:true
+                    });
+                   
+                    `
+                }}
+                />
+                <noscript><div><img src="https://mc.yandex.ru/watch/93342060" style={{position: 'absolute', left: '-9999px'}} alt="" /></div></noscript>
         </SmoothScroll>
     )
 }
